84ed95d9086f100b911fed3622bcd2de5de76f66
DFAM06 <diegoarmu06@gmail.com>
2026-03-11 15:03:49 -0600

    tontería

---
+++ FolderPrueba/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] agrs){
+        pacman();
+    }
+
+    public static void pacman(){
+
+        for (int i = 0; i <= 100000; i++) {
+            System.out.print(":v  -");    
+            if (i%50==0) {
+                System.out.println();
+            }        
+        }
+
+    }
+
+}
